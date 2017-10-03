f4930ce2232021cad10e72f5a4460c2e3e750152
DejanAcim <DejanAcim@gmail.com>
2017-10-03 22:50:48 +0200

    Add getting and listing chosen album data

--- discogs.rb
+++ discogs.rb
@@ -3,8 +3,8 @@ require 'faraday'
 require 'awesome_print'
 
 conn = Faraday.new(:url => 'https://api.discogs.com') do |faraday|
-  faraday.request  :url_encoded             # form-encode POST params
-  faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+  faraday.request  :url_encoded
+  faraday.adapter  Faraday.default_adapter
   faraday.authorization :Discogs, "key=jOUDsbzZVnrDNJmDPIeF, secret=enter_your_secret_here"
 end
 
@@ -14,5 +14,20 @@ end
 body = response.body
 
 json = JSON.parse(body)
-ap json
-puts json.class
+results = json["results"]
+release = results.find { |e| e["type"] == "release" && e["format"].include?("CD") && e["format"].include?("Album") }
+
+url = release["resource_url"]
+puts url
+
+response = conn.get(url)
+album = JSON.parse(response.body)
+
+puts "Tytuł: #{album["title"]}"
+puts "Artysta: #{album["artists"].first["name"]}"
+puts "Gatunek: #{album["genres"].join(", ")}"
+puts "Styl: #{album["styles"].join(". ")}"
+puts "Lista utworów:"
+album["tracklist"].each do |track|
+	puts "#{track["position"]}\t[#{track["duration"]}]\t#{track["title"]}"
+end
