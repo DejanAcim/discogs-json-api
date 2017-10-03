48213cf8f54b74f3b8874216a835e96d9cc303dc
DejanAcim <DejanAcim@gmail.com>
2017-10-03 22:14:01 +0200

    Setup authentication

--- discogs.rb
+++ discogs.rb
@@ -5,11 +5,12 @@ require 'awesome_print'
 conn = Faraday.new(:url => 'https://api.discogs.com') do |faraday|
   faraday.request  :url_encoded             # form-encode POST params
   faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+  faraday.authorization :Discogs, "key=jOUDsbzZVnrDNJmDPIeF, secret=enter_your_secret_here"
 end
 
-## GET ##
-
-response = conn.get '/releases/249504'     # GET http://sushi.com/nigiri/sake.json
+response = conn.get '/database/search', {q: "2pac all eyez on me"} do |request|
+	request.headers['Content-Type'] = "application/json"
+end
 body = response.body
 
 json = JSON.parse(body)
